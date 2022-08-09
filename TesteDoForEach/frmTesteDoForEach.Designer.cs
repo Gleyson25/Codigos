581d48d0a757c753f7615502d1b92dfb3ded87fd
Gleyson Alexandre Leite dos Santos <109443618+Gleyson25@users.noreply.github.com>
2022-08-09 06:49:44 -0300

    Estudo C#

---
+++ TesteDoForEach/frmTesteDoForEach.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TesteDoForEach
+{
+    partial class frmTesteDoForEach
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMensagem = new System.Windows.Forms.Button();
+            this.btnDesvConv = new System.Windows.Forms.Button();
+            this.btnDesvConvEnc = new System.Windows.Forms.Button();
+            this.btnSwitchCase = new System.Windows.Forms.Button();
+            this.btnImpTxtWhile = new System.Windows.Forms.Button();
+            this.lstbxPreferencias = new System.Windows.Forms.ListBox();
+            this.btnImpTxtDoWhile = new System.Windows.Forms.Button();
+            this.btnImpTxtFor = new System.Windows.Forms.Button();
+            this.btnImpTxtForEach = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMensagem
+            // 
+            this.btnMensagem.Location = new System.Drawing.Point(2, 3);
+            this.btnMensagem.Name = "btnMensagem";
+            this.btnMensagem.Size = new System.Drawing.Size(121, 40);
+            this.btnMensagem.TabIndex = 0;
+            this.btnMensagem.Text = "Clique para ver a mensagem";
+            this.btnMensagem.UseVisualStyleBackColor = true;
+            this.btnMensagem.Click += new System.EventHandler(this.btnMensagem_Click);
+            // 
+            // btnDesvConv
+            // 
+            this.btnDesvConv.Location = new System.Drawing.Point(2, 49);
+            this.btnDesvConv.Name = "btnDesvConv";
+            this.btnDesvConv.Size = new System.Drawing.Size(121, 40);
+            this.btnDesvConv.TabIndex = 1;
+            this.btnDesvConv.Text = "Desvio Condicional";
+            this.btnDesvConv.UseVisualStyleBackColor = true;
+            this.btnDesvConv.Click += new System.EventHandler(this.btnDesvConv_Click);
+            // 
+            // btnDesvConvEnc
+            // 
+            this.btnDesvConvEnc.Location = new System.Drawing.Point(2, 95);
+            this.btnDesvConvEnc.Name = "btnDesvConvEnc";
+            this.btnDesvConvEnc.Size = new System.Drawing.Size(121, 40);
+            this.btnDesvConvEnc.TabIndex = 2;
+            this.btnDesvConvEnc.Text = "Desvio Condicional encadeado\\aninhado";
+            this.btnDesvConvEnc.UseVisualStyleBackColor = true;
+            this.btnDesvConvEnc.Click += new System.EventHandler(this.btnDesvConvEnc_Click);
+            // 
+            // btnSwitchCase
+            // 
+            this.btnSwitchCase.Location = new System.Drawing.Point(2, 141);
+            this.btnSwitchCase.Name = "btnSwitchCase";
+            this.btnSwitchCase.Size = new System.Drawing.Size(121, 40);
+            this.btnSwitchCase.TabIndex = 3;
+            this.btnSwitchCase.Text = "Estrutura de escolha";
+            this.btnSwitchCase.UseVisualStyleBackColor = true;
+            this.btnSwitchCase.Click += new System.EventHandler(this.btnSwitchCase_Click);
+            // 
+            // btnImpTxtWhile
+            // 
+            this.btnImpTxtWhile.Location = new System.Drawing.Point(2, 187);
+            this.btnImpTxtWhile.Name = "btnImpTxtWhile";
+            this.btnImpTxtWhile.Size = new System.Drawing.Size(121, 40);
+            this.btnImpTxtWhile.TabIndex = 4;
+            this.btnImpTxtWhile.Text = "Importar Texto While";
+            this.btnImpTxtWhile.UseVisualStyleBackColor = true;
+            this.btnImpTxtWhile.Click += new System.EventHandler(this.btnImpTxtWhile_Click);
+            // 
+            // lstbxPreferencias
+            // 
+            this.lstbxPreferencias.FormattingEnabled = true;
+            this.lstbxPreferencias.Location = new System.Drawing.Point(129, 3);
+            this.lstbxPreferencias.Name = "lstbxPreferencias";
+            this.lstbxPreferencias.Size = new System.Drawing.Size(153, 355);
+            this.lstbxPreferencias.TabIndex = 5;
+            // 
+            // btnImpTxtDoWhile
+            // 
+            this.btnImpTxtDoWhile.Location = new System.Drawing.Point(2, 233);
+            this.btnImpTxtDoWhile.Name = "btnImpTxtDoWhile";
+            this.btnImpTxtDoWhile.Size = new System.Drawing.Size(121, 40);
+            this.btnImpTxtDoWhile.TabIndex = 6;
+            this.btnImpTxtDoWhile.Text = "Importar Texto Do While";
+            this.btnImpTxtDoWhile.UseVisualStyleBackColor = true;
+            this.btnImpTxtDoWhile.Click += new System.EventHandler(this.btnImpTxtDoWhile_Click);
+            // 
+            // btnImpTxtFor
+            // 
+            this.btnImpTxtFor.Location = new System.Drawing.Point(2, 279);
+            this.btnImpTxtFor.Name = "btnImpTxtFor";
+            this.btnImpTxtFor.Size = new System.Drawing.Size(121, 40);
+            this.btnImpTxtFor.TabIndex = 7;
+            this.btnImpTxtFor.Text = "Importar Texto For";
+            this.btnImpTxtFor.UseVisualStyleBackColor = true;
+            this.btnImpTxtFor.Click += new System.EventHandler(this.btnImpTxtFor_Click);
+            // 
+            // btnImpTxtForEach
+            // 
+            this.btnImpTxtForEach.Location = new System.Drawing.Point(2, 321);
+            this.btnImpTxtForEach.Name = "btnImpTxtForEach";
+            this.btnImpTxtForEach.Size = new System.Drawing.Size(121, 40);
+            this.btnImpTxtForEach.TabIndex = 8;
+            this.btnImpTxtForEach.Text = "Importar Texto For Each";
+            this.btnImpTxtForEach.UseVisualStyleBackColor = true;
+            this.btnImpTxtForEach.Click += new System.EventHandler(this.btnImpTxtForEach_Click);
+            // 
+            // frmTesteDoForEach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 366);
+            this.Controls.Add(this.btnImpTxtForEach);
+            this.Controls.Add(this.btnImpTxtFor);
+            this.Controls.Add(this.btnImpTxtDoWhile);
+            this.Controls.Add(this.lstbxPreferencias);
+            this.Controls.Add(this.btnImpTxtWhile);
+            this.Controls.Add(this.btnSwitchCase);
+            this.Controls.Add(this.btnDesvConvEnc);
+            this.Controls.Add(this.btnDesvConv);
+            this.Controls.Add(this.btnMensagem);
+            this.Name = "frmTesteDoForEach";
+            this.Text = "Teste Do ForEach";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMensagem;
+        private System.Windows.Forms.Button btnDesvConv;
+        private System.Windows.Forms.Button btnDesvConvEnc;
+        private System.Windows.Forms.Button btnSwitchCase;
+        private System.Windows.Forms.Button btnImpTxtWhile;
+        private System.Windows.Forms.ListBox lstbxPreferencias;
+        private System.Windows.Forms.Button btnImpTxtDoWhile;
+        private System.Windows.Forms.Button btnImpTxtFor;
+        private System.Windows.Forms.Button btnImpTxtForEach;
+    }
+}
+
---
+++ TesteDoForEach/frmTesteDoForEach.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TesteDoForEach
+{
+    public partial class frmTesteDoForEach : Form
+    {
+        StreamReader objLeitorTxt;
+        string strLinhaLida;
+
+        public frmTesteDoForEach()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMensagem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
+
+        private void btnDesvConv_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Escolha ok ou cancelar!", "Desvio Condicional!", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show("Você escolheu ok!");
+            }
+            else
+            {
+                MessageBox.Show("Você escolheu cancelar!");
+            }
+        }
+
+        private void btnDesvConvEnc_Click(object sender, EventArgs e)
+        {
+            DialogResult ResultadoDoClique = MessageBox.Show("Escolha sim,não ou cancelar!", "Desvio Condicional encadeado!", MessageBoxButtons.YesNoCancel);
+            if (ResultadoDoClique == System.Windows.Forms.DialogResult.Yes)
+            {
+                MessageBox.Show("você escolheu sim!");
+            }
+            else if (ResultadoDoClique == System.Windows.Forms.DialogResult.No)
+            {
+                MessageBox.Show("você escolheu não!");
+            }
+            else
+            {
+                MessageBox.Show("Você escolheu cancelar!");
+            }
+        }
+
+        private void btnSwitchCase_Click(object sender, EventArgs e)
+        {
+            switch (MessageBox.Show("Escolha sim,não ou cancelar!", "Desvio Condicional encadeado!", MessageBoxButtons.YesNoCancel))
+            { 
+                case System.Windows.Forms.DialogResult.Yes:
+                    MessageBox.Show("Você escolheu sim!");
+                    break;
+                case System.Windows.Forms.DialogResult.No:
+                    MessageBox.Show("Você escolheu não!");
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                    MessageBox.Show("Você escolheu cancelar!");
+                    break;
+                default:
+                    MessageBox.Show("Você escolheu errado!,Escolha sim,não ou cancelar");
+                    break;
+            }
+        }
+
+        private void btnImpTxtWhile_Click(object sender, EventArgs e)
+        {
+            lstbxPreferencias.Items.Clear();
+            ImpTxtWhile();
+        }
+        public void ImpTxtWhile()
+        {
+            objLeitorTxt = new StreamReader(@"C:\Curso Programar\Preferencias.txt");
+            strLinhaLida = objLeitorTxt.ReadLine();
+
+            while (strLinhaLida != null)
+            {
+                lstbxPreferencias.Items.Add(strLinhaLida);
+                strLinhaLida = objLeitorTxt.ReadLine();
+            }
+            objLeitorTxt.Close();
+        }
+
+        private void btnImpTxtDoWhile_Click(object sender, EventArgs e)
+        {
+            ImpTxtDoWhile();
+        }
+        public void ImpTxtDoWhile()
+        {
+            objLeitorTxt = new StreamReader(@"C:\Curso Programar\Preferencias.txt");
+            strLinhaLida = objLeitorTxt.ReadLine();
+
+            do
+            {
+                lstbxPreferencias.Items.Add(strLinhaLida);
+                strLinhaLida = objLeitorTxt.ReadLine();
+            } while (strLinhaLida != null);
+            objLeitorTxt.Close();
+        }
+
+        private void btnImpTxtFor_Click(object sender, EventArgs e)
+        {
+            ImpTxtFor();
+        }
+        public void ImpTxtFor()
+        {
+            objLeitorTxt = new StreamReader(@"C:\Curso Programar\Preferencias.txt");
+            
+
+            for(strLinhaLida = objLeitorTxt.ReadLine();strLinhaLida != null;strLinhaLida = objLeitorTxt.ReadLine())
+
+            {
+                lstbxPreferencias.Items.Add(strLinhaLida);
+            }
+            objLeitorTxt.Close();
+        }
+
+        private void btnImpTxtForEach_Click(object sender, EventArgs e)
+        {
+            ImpTxtForEach();
+        }
+        public void ImpTxtForEach()
+        {
+            objLeitorTxt = new StreamReader(@"C:\Curso Programar\Preferencias.txt");
+            string[] arrListaCompleta = objLeitorTxt.ReadToEnd().Split('\n');
+
+            foreach(string itemLido in arrListaCompleta)
+
+            {
+                lstbxPreferencias.Items.Add(itemLido);
+                
+            }
+            objLeitorTxt.Close();
+        }
+    }
+}
